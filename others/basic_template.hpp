48dfe667e23ef10a4bf2364e903bb129f93ce43e
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-07-02 01:31:11 +0900

    テンプレート (短いバージョン) を追加

---
+++ others/basic_template.hpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+#define _overload(_1, _2, _3, name, ...) name
+#define rep2(i, n) rep3(i, 0, n)
+#define rep3(i, a, b) for (ll i = (a); i < (ll)(b); i++)
+#define rep(...) _overload(__VA_ARGS__, rep3, rep2)(__VA_ARGS__)
+#define rrep(i, a, b) for (ll i = a; i >= b; i--)
+#define fore(e, a) for (auto &&e : a)
+#define all(x) (x).begin(), (x).end()
+
+template <typename T>
+using V = vector<T>;
+using ll = long long;
+
+const int dx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+const int dy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+const int INFI = 1 << 30;   // 1.07e9
+const ll INFL = 1ll << 61;  // 2.30e18
+
+/* ------start coding here------ */
+
+
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    
+}
